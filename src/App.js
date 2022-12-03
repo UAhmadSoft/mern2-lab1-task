afbe01b2269226764fb362dd83ace7533b0c6d1a
UAhmadSoft <umadahmad1928@gmail.com>
2022-12-03 12:30:08 +0500

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import AppBar from './components/AppBar';
 import TodoApp from './components/TodoApp';
+import About from './pages/About';
+import ContactUs from './pages/ContactUs';
+import Home from './pages/Home';
 
 function App() {
-  return <div>{/* <TodoApp /> */}</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <AppBar />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/about' element={<About />} />
+          <Route exact path='/contact-us' element={<ContactUs />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
