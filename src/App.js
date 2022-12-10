ebaa9c7cf32874dc0e930a30f19c3ac6ae24fb1c
UAhmadSoft <umadahmad1928@gmail.com>
2022-12-10 18:32:18 +0500

    Added Task

--- src/App.js
+++ src/App.js
@@ -4,54 +4,55 @@ import axios from 'axios';
 import useToggle from './hooks/useToggle';
 import CircularProgress from '@mui/material/CircularProgress';
 import TextField from '@mui/material/TextField';
+import useInput from './hooks/useInput';
+import { Button } from '@mui/material';
+import { Add } from '@mui/icons-material';
 
 function App() {
   // loading state
-  const [loading, toggleLoading] = useToggle(false);
-  const [data, setData] = useState();
 
-  useEffect(() => {
-    (async () => {
-      try {
-        // JSON placeholder API
-        const url = 'https://jsonplaceholder.typicode.com/users';
-        const res = await axios.get(url);
-        let obj = res.data[0];
-        Object.getOwnPropertyNames(obj).forEach(function (prop) {
-          obj[prop] = '';
-        });
-        setData(obj);
-        console.log(res);
-      } catch (err) {
-        console.log(err);
-      } finally {
-        toggleLoading();
-      }
-    })();
-  }, []);
+  // * Generic form Example
+  const [form, setForm] = useState([
+    { id: 1, field: 'name', value: '', type: 'text' },
+    { id: 2, field: 'email', value: '', type: 'email' },
+    { id: 3, field: 'password', value: '', type: 'password' },
+    { id: 4, field: 'confirmPassword', value: '', type: 'password' },
+  ]);
 
-  const handleChange = (e) => {
-    setData((st) => ({ ...st, [e.target.name]: e.target.value }));
+  const [fieldAdded, onFieldChange, resetField] = useInput('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setForm([...form, { id: form.length, field: fieldAdded, value: '' }]);
+    resetField();
   };
 
   return (
     <div>
-      {loading && <CircularProgress size={25} />}
-
-      <form>
-        {data &&
-          Object.keys(data)
-            .filter((el) => el !== 'address' || el !== 'company')
-            .map((el, i) => (
-              <TextField
-                value={data[el]}
-                name={el}
-                onChange={handleChange}
-                key={i}
-                label={el}
-                placeholder={el}
-              />
-            ))}
+      {form.map((item) => (
+        <TextField
+          key={item.id}
+          label={item.field}
+          variant='outlined'
+          value={item.value}
+          type={item.type || 'text'}
+        />
+      ))}
+      <form onSubmit={handleSubmit}>
+        <TextField
+          label='Field Name'
+          variant='outlined'
+          value={fieldAdded}
+          onChange={onFieldChange}
+        />
+        <Button
+          variant='contained'
+          color='success'
+          startIcon={<Add />}
+          type='submit'
+        >
+          Add Field
+        </Button>
       </form>
     </div>
   );
